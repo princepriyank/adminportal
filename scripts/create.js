6491c2d75db5efcb6815bba5c32b7a23d7170bd9
Divya Prakash <medivyaprakash@gmail.com>
2021-04-11 14:54:19 +0530

    bug fix

--- scripts/create.js
+++ scripts/create.js
@@ -29,8 +29,7 @@ async function query(q) {
 
 // Create "entries" table if doesn't exist
 async function migrate() {
-  let queries = [
-    `CREATE TABLE IF NOT EXISTS notices (
+  await query(`CREATE TABLE IF NOT EXISTS notices (
       id bigint NOT NULL,
       title varchar(1000),
       timestamp bigint,
@@ -40,20 +39,9 @@ async function migrate() {
       attachments varchar(1000),
       userId varchar(35) NOT NULL,
       PRIMARY KEY (id)
-    );`,
-    ` CREATE TABLE IF NOT EXISTS events (
-                id bigint NOT NULL,
-                title varchar(1000),
-                timestamp bigint,
-                openDate bigint,
-                closeDate bigint,
-                venue varchar(1000),
-                description varchar(1000),
-                attachments varchar(1000),
-                userId varchar(35) NOT NULL,
-                PRIMARY KEY (id)
-            );`,
-    `create table if not exists news (
+    );`).catch((e) => console.log(e));
+
+  await query(`create table if not exists news (
                 id bigint NOT NULL,
                 title varchar(1000),
                 timestamp bigint,
@@ -64,8 +52,9 @@ async function migrate() {
                 author varchar(1000),
                 userId varchar(35) NOT NULL,
                 PRIMARY KEY (id)
-            );`,
-    `create table if not exists innovation (
+            )`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists innovation (
                 id bigint NOT NULL,
                 title varchar(1000),
                 timestamp bigint,
@@ -76,46 +65,25 @@ async function migrate() {
                 author varchar(1000),
                 userId varchar(35) NOT NULL,
                 PRIMARY KEY (id)
-            );`,
-    ` create table if not exists faculty_image (
+            )`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists faculty_image (
                 user_id int,
                 email varchar(50),
                 image MEDIUMBLOB,
                 PRIMARY KEY(user_id),
                 UNIQUE KEY(email)
-            );`,
-    `create table if not exists education(
-      id int NOT NULL AUTO_INCREMENT,
-                email varchar(100),
-                user_id int NOT NULL,
-                certification varchar(10) NOT NULL,
-                institution text NOT NULL,
-                passing_year varchar(10) DEFAULT NULL,
-                PRIMARY KEY(id)
-            )AUTO_INCREMENT=7781;`,
-    `create table if not exists memberships(
-       id int NOT NULL AUTO_INCREMENT,
-                email varchar(100),
-                user_id int NOT NULL,
-                membership_id varchar(20) NOT NULL,
-                membership_society text NOT NULL,
-                PRIMARY KEY(id)
-            )AUTO_INCREMENT=90000;`,
-    `create table if not exists Project (
+            )`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists Project (
       id int NOT NULL AUTO_INCREMENT,
             user_id int NOT NULL,
             email varchar(250) NOT NULL,
             project text NOT NULL,
             PRIMARY KEY(id)
-    )AUTO_INCREMENT=80000;`,
-    `create table if not exists past_admin_responsibility (
-      id int NOT NULL AUTO_INCREMENT,
-                email varchar(100),
-                user_id int NOT NULL,
-                past_responsibility text NOT NULL,
-                PRIMARY KEY(id)
-    )AUTO_INCREMENT=4500;`,
-    `create table if not exists Phd_Candidates (
+    )AUTO_INCREMENT=80000`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists Phd_Candidates (
             id int NOT NULL AUTO_INCREMENT,
             user_id int NOT NULL,
             email varchar(250) NOT NULL,
@@ -124,30 +92,70 @@ async function migrate() {
             start_year varchar(10) NOT NULL,
             completion_year varchar(10) NOT NULL,
             PRIMARY KEY(id)
-        )AUTO_INCREMENT=120000;`,
-    `create table if not exists Professional_Service (
+        )AUTO_INCREMENT=120000`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists Professional_Service (
             id int NOT NULL AUTO_INCREMENT,
             user_id int NOT NULL,
             email varchar(250) NOT NULL,
             services text NOT NULL,
             PRIMARY KEY(id)
-        )AUTO_INCREMENT=140000;`,
-    `create table if not exists publications (
+        )AUTO_INCREMENT=140000;`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists publications (
                 user_id int NOT NULL,
                 email varchar(50),
                 publication_id int NOT NULL AUTO_INCREMENT,
                 publications mediumtext NOT NULL,
                 PRIMARY KEY(publication_id),
                 UNIQUE KEY(email)
-            )AUTO_INCREMENT=160000;`,
-    `create table if not exists subjects_teaching(
+            )AUTO_INCREMENT=160000`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists subjects_teaching(
                 id int NOT NULL AUTO_INCREMENT,
                 email varchar(100),
                 user_id int NOT NULL,
                 subject text NOT NULL,
                 PRIMARY KEY(id)
-            )AUTO_INCREMENT=20000;`,
-    `create table if not exists users (
+            )AUTO_INCREMENT=20000;`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists Phd_Candidates (
+            id int NOT NULL AUTO_INCREMENT,
+            user_id int NOT NULL,
+            email varchar(250) NOT NULL,
+            phd_student_name text NOT NULL,
+            thesis_topic text NOT NULL,
+            start_year varchar(10) NOT NULL,
+            completion_year varchar(10) NOT NULL,
+            PRIMARY KEY(id)
+        )AUTO_INCREMENT=120000;`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists Professional_Service (
+            id int NOT NULL AUTO_INCREMENT,
+            user_id int NOT NULL,
+            email varchar(250) NOT NULL,
+            services text NOT NULL,
+            PRIMARY KEY(id)
+        )AUTO_INCREMENT=140000;`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists publications (
+                user_id int NOT NULL,
+                email varchar(50),
+                publication_id int NOT NULL AUTO_INCREMENT,
+                publications mediumtext NOT NULL,
+                PRIMARY KEY(publication_id),
+                UNIQUE KEY(email)
+            )AUTO_INCREMENT=160000;`).catch((e) => console.log(e));
+
+  await query(`create table if not exists subjects_teaching(
+                id int NOT NULL AUTO_INCREMENT,
+                email varchar(100),
+                user_id int NOT NULL,
+                subject text NOT NULL,
+                PRIMARY KEY(id)
+            )AUTO_INCREMENT=20000;`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists users (
                 id int NOT NULL AUTO_INCREMENT,
                 name varchar(50),
                 email varchar(100),
@@ -159,18 +167,66 @@ async function migrate() {
                 PRIMARY KEY (id),
                 UNIQUE KEY (email)
             )AUTO_INCREMENT=1000;
-`,
-    `create table if not exists Work_Experience (
+`).catch((e) => console.log(e));
+  
+   await query(`create table if not exists Work_Experience (
             id int NOT NULL AUTO_INCREMENT,
             user_id int NOT NULL,
             email varchar(250) NOT NULL,
             work_experiences text NOT NULL,
             PRIMARY KEY(id)
-        )AUTO_INCREMENT=60000;`,
-  ];
-  queries.forEach(async (element) => {
-    await query(`${element}`).catch((e) => console.log(e));
-  });
+        )AUTO_INCREMENT=60000`).catch((e) => console.log(e));
+
+  await query(`create table if not exists curr_admin_resonsibility (
+                id int NOT NULL AUTO_INCREMENT,
+                email varchar(100),
+                user_id int NOT NULL,
+                curr_responsibility text NOT NULL,
+                PRIMARY KEY(id)
+            )AUTO_INCREMENT=34000;`).catch((e) => console.log(e));
+  
+  await query(`CREATE TABLE if not exists past_admin_responsibility (
+                id int NOT NULL AUTO_INCREMENT,
+                email varchar(100),
+                user_id int NOT NULL,
+                past_responsibility text NOT NULL,
+                PRIMARY KEY(id)
+            ) AUTO_INCREMENT = 4500;`).catch((e) => console.log(e));
+  
+  await query(`CREATE TABLE if not exists education (
+                id int NOT NULL AUTO_INCREMENT,
+                email varchar(100),
+                user_id int NOT NULL,
+                certification varchar(10) NOT NULL,
+                institution text NOT NULL,
+                passing_year varchar(10) DEFAULT NULL,
+                PRIMARY KEY(id)
+            )AUTO_INCREMENT=7781`).catch((e) => console.log(e));
+  
+  await query(`create table if not exists events (
+                id bigint NOT NULL,
+                title varchar(1000),
+                timestamp bigint,
+                openDate bigint,
+                closeDate bigint,
+                venue varchar(1000),
+                doclink varchar(500),
+                attachments varchar(1000),
+                userId varchar(35) NOT NULL,
+                PRIMARY KEY (id))`).catch((e) => console.log(e));
+  
+  await query(`CREATE TABLE if not exists memberships (
+                id int NOT NULL AUTO_INCREMENT,
+                email varchar(100),
+                user_id int NOT NULL,
+                membership_id varchar(20) NOT NULL,
+                membership_society text NOT NULL,
+                PRIMARY KEY(id)
+            )AUTO_INCREMENT=90000;`).catch((e) => console.log(e));
+  
+                
+  
+  
   console.log("migration ran successfully");
 }
 
